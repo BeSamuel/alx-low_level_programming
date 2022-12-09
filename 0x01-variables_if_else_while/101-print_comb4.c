39d6706a483c7274831ed9e0492c9161b1675d2d
BeSamuel <samuelasefa27@gmail.com>
2022-12-09 12:17:41 -0800

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,30 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - entry block
- *
+ * main - main block
+ * Description: Write a programe that prints all posible
+ * different combinatons of two digits
+ * Numbers must be separeted by ,, followed by a space
+ * The two digits must be different
+ * 01 and 10 are consider the same  combination of two digits 0 and 1
+ * Print only the smallest compination of two digits
+ * Numbers should be printed function in ascending order, with two digits
+ * You can only use the puncher in function
+ * (every other function (printf, puts, etc..) is forbidden)
  * Return: 0
- **/
+ */
 int main(void)
 {
-	int i, j;
+	int c;
+	int d;
+	int e = 0;
 
-	for (i = '0'; i <= '9'; i++)
+	while (e <10)
 	{
-		for (j = i + 1; j <= '9'; j++)
+		d =0;
+		while (d < 10)
 		{
-			for (K = j + 1; k <= '9'; k++)
+			c = 0;
+			while (c < 10)
 			{
-				putchar(i);
-				putchar(j);
-				putchar(k);
-				if (i != '7' || j != '8' || k != '9')
+				if (c != d && d != e < d && d <c)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar('0' + e);
+					putchar('0' + d);
+					putchar('0' + c);
+
+					if (c + d + e != 9 + 8 + 7 )
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
 				}
+				c++;
 			}
+			d++;
 		}
+		e++;
 	}
 	putchar('\n');
 	return (0);
 }
+
