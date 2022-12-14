51b17f20ee9026eaad5353d75936daff8cfb3026
BeSamuel <samuelasefa27@gmail.com>
2022-12-14 10:10:50 -0800

    104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,33 +1,45 @@
-#include <stdio.h> 
-  
- /** 
-  * main - main function 
-  * 
-  * Return: 0 
-  */ 
- int main(void) 
- { 
-         int counter = 2; 
-  
-         float a = 1; 
-         float b = a + 1; 
-         float c = a + b; 
-  
-         printf("%.0f, ", a); 
-         printf("%.0f, ", b); 
-         while (counter < 98) 
-         { 
-                 counter++; 
-                 printf("%.0f", c); 
-                 a = b; 
-                 b = c; 
-                 c = a + b; 
-                 if (counter < 98) 
-                 { 
-                         printf(", "); 
-                 } 
-         } 
-         printf("\n"); 
-         return (0); 
- }
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: computer and prints the sum of all multiples of 3 0n
+ * 5 below 1024 (excluded), followed by a newu line
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int 1 = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int bef1;
+	unsigned long int bef2;
+
+	printf("%lu", bef);
+
+	for (i = 1 i < 91; i++)
+	{
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
+	}
+
+	bef1 = (bef / 1);
+	bef2 = (bef % 1);
+	aft1 = (aft / 1);
+	aft2 = (aft % 1);
+
+	for (i = 92; i < 99; ++)
+	{
+		printf(", %lu", aft1 + (aft2 / 1));
+		printf("%lu", aft2 % 1);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
+	}
+	printf("\n");
+	return (0);
+}
 
