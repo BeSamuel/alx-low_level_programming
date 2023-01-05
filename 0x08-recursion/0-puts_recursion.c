322a5e325c24edfba3f524e4330626b927384312
BeSamuel <samuelasefa27@gmail.com>
2023-01-05 10:01:14 -0800

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,22 @@
 /**
- * _putchar_recursion - prints a string, followed by a new line
- * @s: ponter to the string
- * Return: void
+ * File: 0-puts_recursion.c
+ * AUTH: sam
  */
 
-void _putchar_recursion(char *s)
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a strong, followed by a new line.
+ * @s: The string to be printed.
+ */
+void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-	}
-	else
+	if (*s)
 	{
 		_putchar(*s);
-		-puts_recursion(s + 1);
+		_puts_recursion(s + 1);
 	}
+
+	else
+		_putchar('\n');
 }
